85043fcee202fb2285642062ad0af61e50559baf
Thorsten Hack <thorstenhack@gmx.de>
2021-09-28 15:25:17 +0200

    Add referrer tracking

---
+++ app/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$ref = array_key_exists("HTTP_REFERER", $_SERVER) ? $_SERVER['HTTP_REFERER'] : null;
+
+if($ref) {
+	require_once '../backend/config.php';
+	require_once '../backend/core/db/DB.php';
+
+
+	DB::init(DB_HOST, DB_USER, DB_PASSWORD, 'analytics');
+
+	DB::insert("app_referrer", [
+		"referrer" => $ref,
+		"timestamp" => time()
+	]);
+}
+
+header('Location: https://www.subscribepage.com/e7c7n2');
+exit;
+
+?>
